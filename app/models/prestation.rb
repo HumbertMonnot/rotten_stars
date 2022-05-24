382155273c0acb8204aabfd6119672c4961055a6
nicolas perier <nicolas_perier@mailo.com>
2022-05-24 11:01:43 +0200

    prestation model created

---
+++ app/models/prestation.rb
@@ -0,0 +1,12 @@
+class Prestation < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  # mettre une array de solution prédéfinies
+  validates :category, inclusion: { in: ['sing', 'danse'] }
+  validates :description, presence: true, length: { in: 150..300 }
+  validates :price, numericality: true
+  validates :address, presence: true
+  validates :distance, numericality: true
+  validates :punchline, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_05_24_084645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "prestations", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.text "description"
+    t.integer "price"
+    t.string "address"
+    t.integer "distance"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "punchline"
+  end
 
 end
