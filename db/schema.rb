33292f3ed9a51a95a5aa3c8b4ea112c56c512095
humbertmonnot <humbert.monnot@gmail.com>
2022-05-24 11:48:38 +0200

    with reservation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-  ActiveRecord::Schema.define(version: 2022_05_24_084645) do
+ActiveRecord::Schema.define(version: 2022_05_24_093417) do
 
-    # These are extensions that must be enabled in order to support this database
-    enable_extension "plpgsql"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
-    create_table "prestations", force: :cascade do |t|
-      t.string "name"
-      t.string "category"
-      t.text "description"
-      t.integer "price"
-      t.string "address"
-      t.integer "distance"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-      t.string "punchline"
-    end
-
-    create_table "users", force: :cascade do |t|
-      t.string "email", default: "", null: false
-      t.string "encrypted_password", default: "", null: false
-      t.string "reset_password_token"
-      t.datetime "reset_password_sent_at"
-      t.datetime "remember_created_at"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-      t.string "first_name"
-      t.string "last_name"
-      t.string "phone_number"
-      t.index ["email"], name: "index_users_on_email", unique: true
-      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    end
+  create_table "prestations", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.text "description"
+    t.integer "price"
+    t.string "address"
+    t.integer "distance"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "punchline"
   end
+
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "prestation_id", null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.integer "price"
+    t.integer "total"
+    t.string "state"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["prestation_id"], name: "index_reservations_on_prestation_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "reservations", "prestations"
+  add_foreign_key "reservations", "users"
+end
