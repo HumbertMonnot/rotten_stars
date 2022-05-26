461f084c223b5d3cba10f0c2e687b40b397905c5
Oceane Feltrin <oceanefeltrin@yahoo.fr>
2022-05-26 18:09:07 +0200

    filter category implemented

--- app/javascript/controllers/total_controller.js
+++ app/javascript/controllers/total_controller.js
@@ -1,12 +1,15 @@
-const end_date = document.getElementById("reservation_end_date");
-const start_date = document.getElementById("reservation_start_date")
-const prestation = document.getElementById("prestation-price")
-const total = document.getElementById("reservation-total")
+document.addEventListener('turbolinks:load', () => {
 
-if (end_date) {
-  end_date.addEventListener("input", () => {
-    duration = (Date.parse(end_date.value) - Date.parse(start_date.value)) / (1000 * 3600 * 24);
-    reservation_total = parseInt(prestation.innerText) * duration;
-    total.innerText = reservation_total;
-  });
-}
+  const end_date = document.getElementById("reservation_end_date");
+  const start_date = document.getElementById("reservation_start_date")
+  const prestation = document.getElementById("prestation-price")
+  const total = document.getElementById("reservation-total")
+
+  if (end_date) {
+    end_date.addEventListener("input", () => {
+      duration = (Date.parse(end_date.value) - Date.parse(start_date.value)) / (1000 * 3600 * 24) + 1;
+      reservation_total = parseInt(prestation.innerText) * duration;
+      total.innerText = reservation_total;
+    });
+  }
+})
