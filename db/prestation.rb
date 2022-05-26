a97d3dd024a920274a561d2b3a3009b37d819749
BELABBAS FARAH <farah.belabbas@gmail.com>
2022-05-26 16:55:55 +0200

    db seed prestation

---
+++ db/prestation.rb
@@ -0,0 +1,125 @@
+presta_1 = Prestation.new(
+  name: "LOANA & PISCINE",
+  category: "Soirée",
+  description: "J’anime toutes tes soirées ! Seul besoin : Une piscine ! Confiez-moi votre prestation plaisir, un seul objectif vous divertir. Les différents thèmes abordés durant cette soirée seront la danse, les sports aquatiques et plus encore !
+  ",
+  price: 1300,
+  address: "5 rue de bouliac, 33100 Bordeaux",
+  distance: 32,
+  punchline: "J'ANIME TES POOLS PARTY",
+  file: https://static.jeanmarcmorandini.com/sites/jeanmarcmorandini.com/files/styles/image_750_324/public/692450efe864df3ccaa07b96d906c1fc_29.jpg,
+)
+
+  presta_2 = Prestation.new(
+    name: "MA GLOIRE L'ANIMATEUR",
+    category: "Entreprise",
+    description: "You need me ! pour animer vos évènements d’entreprise. Pourquoi prendre un animateur ? La réponse est simple. J’utilise une variété de techniques pour créer l'illusion de mouvement et donner aux objets un aspect tridimensionnel et tout cela avec des paillettes ! ",
+    price: 5000,
+    address: "Avenue du Souvenir, 33700 Mérignac",
+    distance: 60,
+    punchline: "NE LOUPER PLUS VOS EVENEMENTS PROS",
+    file: https://republiquedujapap.com/wp-content/uploads/2021/11/6693001-magloire-lors-de-la-soiree-le-tremplin-d-opengraph_1200-3.jpg?ezimgfmt=ng:webp/ngcb18
+  )
+
+  presta_3 = Prestation.new(
+    name: "LA BOGOSSITUDE EN TOI",
+    category: "Soirée",
+    description: "Je prône un concept la Bogossitude. Si tu as besoin d’avoir un BG qui ressemble à Brad Pitt pour le corps, Napoléon pour l'ambition et Christophe Colomb pour l'esprit de conquête. Compte sur moi pour vous faire passer un bon moment! ",
+    price: 2000,
+    address: "11 Rue Pierre Benoît, 33500 Libourne",
+    distance: 45,
+    punchline: "JE VOUS AIDE A VOUS SENTIR MIEUX",
+    file: https://www.exceptionn-elle.fr/wp-content/upLoads/mickael-vendetta.jpg
+  )
+
+  presta_4 = Prestation.new(
+    name: "Amandine du 38 - RAPPEUSE",
+    category: "Chanson",
+    description: "Tu te souviens de mon flow ? Depuis mon déménagement en région bordelaise, je propose mes services pour animer vos mariages",
+    price: 10,
+    address: "11 Rue Pierre Benoît, 33500 Libourne",
+    distance: 45,
+    punchline: "UN PETIT CONCERT POUR TES BRO'",
+    file: https://www.lesinrocks.com/wp-content/uploads/2014/12/amandine.jpg?resize=600%2C400&quality=75
+  )
+
+
+  presta_5 = Prestation.new(
+    name: "SHIRLEY & DINO - LA RETRAITE",
+    category: "Humour",
+    description: "Ok, nous sommes un peu génant, mais en vrai on fait les parents alors n'hésite pas à nous solliciter pour des joyeuses journées !",
+    price: 1500,
+    address: "5 rue luckner, 33000 Bordeaux",
+    distance: 45,
+    punchline: "UN SPECTACLE POUR LES RETRAITES",
+    file: https://images.ladepeche.fr/api/v1/images/view/5c376f2f3e454661f5778cf1/large/image.jpg
+  )
+
+
+  presta_6 = Prestation.new(
+    name: "LOANA & LA COIFFURE",
+    category: "Soirée",
+    description: "Coucou si tu souhaites animer une soirée entre copines, je peux te proposer une soirée 'mèches', je prends tous le matos avec moi !",
+    price: 1250,
+    address: "5 rue de bouliac, 33100 Bordeaux",
+    distance: 45,w
+    punchline: "DES MECHES ROSES POUR SEDUIRE",
+    file: https://scontent-rtl.akamaized.net/GED/01110000/1115700/1115723_700x0.webp
+  )
+
+
+  presta_7 = Prestation.new(
+    name: "  Beber Le Frelon & son show",
+    category: "Soirée",
+    description: "Tu veux faire illusion ? Je débarque et j'anime une partie de votre soirée. Je prends mon déguisement, ma plus belle voix et je me dandine pendant 1h!",
+    price: 1250,
+    address: "14 bis rue du Professeur-Calmette 33140 Villenave-d'Ornon",
+    distance: 45,
+    punchline: "JE SUIS HABILLE EN FRELON",
+    file: https://i62.servimg.com/u/f62/19/44/18/93/1562fv10.jpg
+  )
+
+  presta_8 = Prestation.new(
+    name: "  Beber Le Frelon & son show",
+    category: "Soirée",
+    description: "Tu veux faire illusion ? Je débarque et j'anime une partie de votre soirée. Je prends mon déguisement, ma plus belle voix et je me dandine pendant 1h!",
+    price: 1250,
+    address: "14 bis rue du Professeur-Calmette 33140 Villenave-d'Ornon",
+    distance: 35,
+    punchline: "JE SUIS HABILLE EN FRELON",
+    file: https://i62.servimg.com/u/f62/19/44/18/93/1562fv10.jpg
+  )
+
+  presta_9 = Prestation.new(
+    name: " MOUNDIR & LA COMPASSION",
+    category: "Soirée",
+    description: "Tu as besoin que je t'écoute te plaindre pendant 2 heures ? Choisis cette prestation !",
+    price: 590,
+    address: "6 avenue Pasteur BP 110 33271 Floirac",
+    distance: 55,
+    punchline: "JE FERME MA GUEULE PENDANT 2 HEURES",
+    file: https://i62.servimg.com/u/f62/19/44/18/93/1562fv10.jpg
+  )
+
+
+  presta_9 = Prestation.new(
+    name: "MOUNDIR & LA CHANSON",
+    category: "Soirée",
+    description: "J'anime tes soirées karaokés. Une seule règle, tu chantes mal et je t'explose la gueule!",
+    price: 790,
+    address: "6 avenue Pasteur BP 110 33271 Floirac",
+    distance: 55,
+    punchline: "J'ANIME LES KARAOKES'",
+    file: https://static1.ozap.com/articles/7/58/77/07/@/4627600-moundir-reprend-difficilement-une-chanso-article_media_image-3.jpg
+  )
+
+  presta_9 = Prestation.new(
+    name: "MOUNDIR & LA CLASSE",
+    category: "Autre",
+    description: "J'anime ton mariage ! Tu me prépares le discours et les documents et je viens pendant 50 minutes après c'est trop long pour les invités!",
+    price: 590,
+    address: "6 avenue Pasteur BP 110 33271 Floirac",
+    distance: 59,
+    punchline: "MC A TON MARIAGE",
+    file: https://static1.ozap.com/articles/7/58/77/07/@/4627600-moundir-reprend-difficilement-une-chanso-article_media_image-3.jpg
+  )
