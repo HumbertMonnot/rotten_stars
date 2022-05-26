309e6f0b786d1cb752f81108723097e0dc1c7c63
Oceane Feltrin <oceanefeltrin@yahoo.fr>
2022-05-26 14:31:03 +0200

    total JS done on prestation show

--- app/javascript/controllers/total_controller.js
+++ app/javascript/controllers/total_controller.js
@@ -1,6 +1,12 @@
-const total = document.getElementById("reservation_end_date");
-total.addEventListener("click", () => {
-  duration = (reservation.end_date - reservation.start_date).to_i / (3600 * 24) + 1;
-  reservation_total = prestation.price * duration;
-  console.log(`Total: ${reservation_total}€`);
-});
+const end_date = document.getElementById("reservation_end_date");
+const start_date = document.getElementById("reservation_start_date")
+const prestation = document.getElementById("prestation-price")
+const total = document.getElementById("reservation-total")
+
+if (end_date) {
+  end_date.addEventListener("input", () => {
+    duration = (Date.parse(end_date.value) - Date.parse(start_date.value)) / (1000 * 3600 * 24);
+    reservation_total = parseInt(prestation.innerText) * duration;
+    total.innerText = reservation_total;
+  });
+}
